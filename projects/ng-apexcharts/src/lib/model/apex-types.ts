db5c5f845f5cf913934dbc870cae1fd88d320fdf
Juned Chhipa <juned.chhipa@gmail.com>
2019-09-09 20:38:26 +0530

    missing type added

--- projects/ng-apexcharts/src/lib/model/apex-types.ts
+++ projects/ng-apexcharts/src/lib/model/apex-types.ts
@@ -63,6 +63,7 @@ export interface ApexChart {
     beforeMount?(chart: any, options: any): void;
     mounted?(chart: any, options: any): void;
     updated?(chart: any, options: any): void;
+    mouseMove?(e: any, chart: any, options: any): void;
     click?(e: any, chart: any, options: any): void;
     legendClick?(chart: any, seriesIndex: number, options: any): void;
     markerClick?(e: any, chart: any, options: any): void;
