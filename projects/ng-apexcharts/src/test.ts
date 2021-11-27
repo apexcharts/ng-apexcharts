f57fc54b5e26cfb816a499fc7a767139119b58a2
Arthur Ming <guobin.ming@pwc.com>
2021-11-27 16:34:14 +0800

    test: fix test error

--- projects/ng-apexcharts/src/test.ts
+++ projects/ng-apexcharts/src/test.ts
@@ -1,12 +1,11 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
-import 'core-js/es7/reflect';
 import 'zone.js';
 import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
 import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
+  platformBrowserDynamicTesting,
+  BrowserDynamicTestingModule
 } from '@angular/platform-browser-dynamic/testing';
 
 declare const require: any;
