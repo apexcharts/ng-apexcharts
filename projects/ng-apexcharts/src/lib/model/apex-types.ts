6f1aa02cc7ad338745428bc645b880f4f892fcf3
junedchhipa <juned.chhipa@gmail.com>
2022-10-12 12:57:01 +0530

    fix type for radialbar track background

--- projects/ng-apexcharts/src/lib/model/apex-types.ts
+++ projects/ng-apexcharts/src/lib/model/apex-types.ts
@@ -608,7 +608,7 @@ export interface ApexPlotOptions {
       show?: boolean;
       startAngle?: number;
       endAngle?: number;
-      background?: string;
+      background?: string | string[];
       strokeWidth?: string;
       opacity?: number;
       margin?: number;
