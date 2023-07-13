64f5e94a378c5e7cdbaa375b6b078ff2d5983322
junedchhipa <juned.chhipa@gmail.com>
2023-07-13 19:15:46 +0530

    upgrade to v15

--- projects/ng-apexcharts/src/test.ts
+++ projects/ng-apexcharts/src/test.ts
@@ -1,23 +1,18 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
-import 'zone.js';
-import 'zone.js/testing';
-import { getTestBed } from '@angular/core/testing';
+import "zone.js";
+import "zone.js/testing";
+import { getTestBed } from "@angular/core/testing";
 import {
   platformBrowserDynamicTesting,
-  BrowserDynamicTestingModule
-} from '@angular/platform-browser-dynamic/testing';
-
-declare const require: any;
+  BrowserDynamicTestingModule,
+} from "@angular/platform-browser-dynamic/testing";
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting(), {
-    teardown: { destroyAfterEach: false }
-}
+  platformBrowserDynamicTesting(),
+  {
+    teardown: { destroyAfterEach: false },
+  }
 );
-// Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
-// And load the modules.
-context.keys().map(context);
