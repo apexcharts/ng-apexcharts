fe9d472a5926ad43d07ee7b409300b5c303c91b5
brampeirs <bram.peirs@showpad.com>
2025-11-21 11:23:18 +0100

    update to angular 21

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,8 +1,8 @@
-import { bootstrapApplication } from "@angular/platform-browser";
+import { bootstrapApplication, BootstrapContext } from "@angular/platform-browser";
 
 import { AppComponent } from "./app/app.component";
 import { appConfig } from "./app/app.config";
 
-const bootstrap = () => bootstrapApplication(AppComponent, appConfig);
+const bootstrap = (context: BootstrapContext) => bootstrapApplication(AppComponent, appConfig, context);
 
 export default bootstrap;
